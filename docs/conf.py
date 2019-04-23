b39a742fe61abbb06f4515dfc08790310828ea79
Frank Zickert <webmaster@clued-up.de>
2019-04-23 12:42:30 +0200

    added doc

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,3 @@
 html_theme_options = {
-    'html_title':'Infrastructure-Components',
-    'project': 'Infrastructure-Components',
-    'author': 'Dr. Frank Zickert',
-    'copyright': '2019, Frank Zickert',
-    #'canonical_url': '',
-    #'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    #'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    #'vcs_pageview_mode': '',
-    #'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+
 }
